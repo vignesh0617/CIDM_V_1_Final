0c9ebf65ce85cd3c4cc1d5cb988ae2eb00ba3b99
Vignesh.R <vignesh171099@gmail.com>
2024-04-05 09:24:59 +0530

    installation changes

--- installation.py
+++ installation.py
@@ -2,6 +2,20 @@ import os
 
 print("Going to install required packages .....")
 
-os.system('pip install -r requirements.txt')
+try:
+    os.system('pip install mysql-connector-python')
+    os.system('pip install -r requirements.txt')
+except Exception as e:
+    print(f'''\n\n\n
+      ********************************************************************
+      *---------------------------Error!!---------------------------------*
+      ********************************************************************
+      \n\n\n''')
+    input(e,"\n\n\nClick any button to exit")
+    
 
-input("Required packages installed. Click any button to exit")
+input(f'''\n\n\n
+      ********************************************************************
+      *-----Required packages installed. Click any button to exit--------*
+      ********************************************************************
+      \n\n\n''')
